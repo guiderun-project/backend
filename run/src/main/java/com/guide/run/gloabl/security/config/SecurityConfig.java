9fec4d1b621a199f19160012c5ee4014b4839f3a
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2024-01-05 21:23:26 +0900

    jwt를 통한 시큐리티 인증 구현

--- run/src/main/java/com/guide/run/gloabl/security/config/SecurityConfig.java
+++ run/src/main/java/com/guide/run/gloabl/security/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.guide.run.gloabl.security.config;
 
+import com.guide.run.gloabl.jwt.JwtAuthenticationFilter;
+import com.guide.run.gloabl.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
+    private final JwtProvider jwtProvider;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
         http
@@ -16,8 +22,10 @@ public class SecurityConfig {
                 .sessionManagement(httpSecuritySessionManagementConfigurer ->
                         httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/**").permitAll()
-                );
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll())
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider),
+                        UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
