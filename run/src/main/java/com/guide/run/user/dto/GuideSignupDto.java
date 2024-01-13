1ff7cc44308b08d51441a5307646bd64ba1c7e8b
2ralumin <2ralumin@naver.com>
2024-01-13 21:36:14 +0900

    feat(GuideSignupDto): guide 회원가입 dto 생성

---
+++ run/src/main/java/com/guide/run/user/dto/GuideSignupDto.java
@@ -0,0 +1,26 @@
+package com.guide.run.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GuideSignupDto {
+    private String name;
+    private String gender;
+    private String phoneNumber;
+    private boolean openNumber;
+    private int age;
+    private String detailRecord;
+    private String recordDegree; //개인 기록
+    private String snsId;
+    private boolean openSNS;
+
+    private boolean guideExp;
+    private String viName;
+    private String viRecord;
+    private String viCount; //상세한 가이드 경험
+    private String guidingPace; //가이드 가능한 페이스 그룹
+}
