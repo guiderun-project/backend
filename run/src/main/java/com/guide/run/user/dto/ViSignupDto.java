64a465aac26e2549620e349ccf1a8cce919217bc
2ralumin <2ralumin@naver.com>
2024-01-13 21:33:04 +0900

    feat(ViSignupDto): vi 회원가입용 dto 클래스 필드 수정.

--- run/src/main/java/com/guide/run/user/dto/ViSignupDto.java
+++ run/src/main/java/com/guide/run/user/dto/ViSignupDto.java
@@ -1,5 +1,8 @@
 package com.guide.run.user.dto;
 
+import com.guide.run.user.entity.type.Role;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,13 +11,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ViSignupDto {
+    //private String accountId;
+    //private String password;
     private String name;
     private String gender;
     private String phoneNumber;
+    private boolean openNumber;
     private int age;
     private String detailRecord;
     private String recordDegree; //개인 기록
     private String snsId;
+    private boolean openSNS;
     private boolean runningExp;
     private String guideName;
 }
