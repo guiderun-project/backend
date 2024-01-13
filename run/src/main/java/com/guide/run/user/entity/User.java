9967921e6cb710969d1e378d3e17691d2ae6cd2e
2ralumin <2ralumin@naver.com>
2024-01-13 17:48:41 +0900

    refactor(User): User entity에 있던 userId 를 유니크 키로 설정.

--- run/src/main/java/com/guide/run/user/entity/User.java
+++ run/src/main/java/com/guide/run/user/entity/User.java
@@ -19,6 +19,7 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String userId;
     private String name;
     private String gender;
