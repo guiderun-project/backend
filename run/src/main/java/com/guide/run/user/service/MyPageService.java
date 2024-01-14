9c6f59a2d8f89b5ab67d21994af39910421f1882
2ralumin <2ralumin@naver.com>
2024-01-14 18:02:56 +0900

    feat(MyPageService): UserService가 너무 긴 것 같아 MyPageService 클래스를 생성 후 약관 조희 기능을 구현

---
+++ run/src/main/java/com/guide/run/user/service/MyPageService.java
@@ -0,0 +1,34 @@
+package com.guide.run.user.service;
+
+import com.guide.run.user.dto.PermissionDto;
+import com.guide.run.user.entity.Permission;
+import com.guide.run.user.repository.ArchiveDataRepository;
+import com.guide.run.user.repository.PermissionRepository;
+import com.guide.run.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class MyPageService {
+    private final UserRepository userRepository;
+    private final PermissionRepository permissionRepository;
+    private final ArchiveDataRepository archiveDataRepository;
+
+    public PermissionDto getPermission(String userId){
+        Permission permission = permissionRepository.findById(userId).orElseThrow(
+                () -> new NoSuchElementException()
+        );
+
+        PermissionDto response = PermissionDto.builder()
+                .privacy(permission.isPrivacy())
+                .portraitRights(permission.isPortraitRights())
+                .build();
+
+        return response;
+    }
+}
