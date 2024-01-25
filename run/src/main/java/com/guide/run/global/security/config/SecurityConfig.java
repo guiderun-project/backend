4bbc973d4e12f166fe2b75a3d2a878bbc1bc523f
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2024-01-26 00:45:43 +0900

    feat(SecurityConfig,EventService) event update 관련 에러 추가,security 로그인 부분 설정 해제 -해당 이벤트가 존재하지 않는 경우 에러 코드 추가 -로그인 api 요청 security 해제

--- run/src/main/java/com/guide/run/global/security/config/SecurityConfig.java
+++ run/src/main/java/com/guide/run/global/security/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -16,6 +17,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
     private final JwtProvider jwtProvider;
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return web -> {
+            web.ignoring()
+                    .requestMatchers("/api/oauth/**");
+        };
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
         http
