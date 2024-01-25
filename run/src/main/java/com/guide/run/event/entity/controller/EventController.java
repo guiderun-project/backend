9e1f5f31aa804c792d8a3f8182fc15e1a37c3ace
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2024-01-25 23:00:32 +0900

    feat(EventController) 에러코드 추가 test(EventServiceTest) event생성 테스트 코드 작성

--- run/src/main/java/com/guide/run/event/entity/controller/EventController.java
+++ run/src/main/java/com/guide/run/event/entity/controller/EventController.java
@@ -3,7 +3,11 @@ package com.guide.run.event.entity.controller;
 import com.guide.run.event.entity.dto.request.EventCreateRequest;
 import com.guide.run.event.entity.dto.response.EventCreatedResponse;
 import com.guide.run.event.entity.service.EventService;
+import com.guide.run.global.exception.admin.authorize.NotAuthorityAdminException;
+import com.guide.run.global.exception.user.resource.NotExistUserException;
 import com.guide.run.global.jwt.JwtProvider;
+import com.guide.run.user.entity.User;
+import com.guide.run.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,15 +17,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.guide.run.user.entity.type.Role.VADMIN;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/event")
 public class EventController {
     private final JwtProvider jwtProvider;
     private final EventService eventService;
+    private final UserRepository userRepository;
     @PostMapping
     public ResponseEntity<EventCreatedResponse> eventCreate(@RequestBody EventCreateRequest eventCreateRequest, HttpServletRequest request){
         String userId = jwtProvider.resolveToken(request);
+        User user = userRepository.findByUserId(userId).
+                orElseThrow(() -> new NotExistUserException());
+        if(user.getRole().getValue() != VADMIN.getValue())
+            throw new NotAuthorityAdminException();
         EventCreatedResponse eventCreatedResponse = eventService.eventCreate(eventCreateRequest, userId);
         return ResponseEntity.status(HttpStatus.CREATED).body(eventCreatedResponse);
     }
