2aad82ed9ff2aa1d163e1285ba7011dd8f751657
2ralumin <2ralumin@naver.com>
2024-01-26 22:58:30 +0900

    refactor(Dto) : 필드 타입이 변경되면서 각 dto에 howToKnow 타입 변경

--- run/src/main/java/com/guide/run/user/entity/ArchiveData.java
+++ run/src/main/java/com/guide/run/user/entity/ArchiveData.java
@@ -1,10 +1,14 @@
 package com.guide.run.user.entity;
 
+import com.guide.run.global.converter.StringListConverter;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -15,7 +19,12 @@ public class ArchiveData {
     @Id
     private String userId;
     private String runningPlace;
-    private String howToKnow;
+
+    @Convert(converter = StringListConverter.class)
+    private List<String> howToKnow = new ArrayList<>();
+
+
     private String motive;
+    private String hopePrefs;
 
 }
