60cdbedf404261c6d2cd2d6562e6c53e2ba48ed5
2ralumin <2ralumin@naver.com>
2024-01-26 23:01:58 +0900

    feat(UserInfoController) : api 문서에 맞게 경로 수정. 러닝 스펙 조회 api 구현. userId로 조회할 수 있도록 코드 수정해야 함.

--- run/src/main/java/com/guide/run/user/controller/UserInfoController.java
+++ run/src/main/java/com/guide/run/user/controller/UserInfoController.java
@@ -1,8 +1,10 @@
 package com.guide.run.user.controller;
 
 import com.guide.run.global.jwt.JwtProvider;
+import com.guide.run.user.dto.GuideRunningInfoDto;
 import com.guide.run.user.dto.PermissionDto;
 import com.guide.run.user.dto.PersonalInfoDto;
+import com.guide.run.user.dto.ViRunningInfoDto;
 import com.guide.run.user.service.UserInfoService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -16,28 +18,49 @@ public class UserInfoController {
     private final UserInfoService userInfoService;
     private final JwtProvider jwtProvider;
 
-    @GetMapping("/mypage/info/permission")
-    public ResponseEntity<PermissionDto> getPermission(HttpServletRequest httpServletRequest){
-        String userId = jwtProvider.extractUserId(httpServletRequest);
+    @GetMapping("/user/permission/{userId}")
+    public ResponseEntity<PermissionDto> getPermission(String userId,
+                                                       HttpServletRequest httpServletRequest){
+        String signupId = jwtProvider.extractUserId(httpServletRequest);
         PermissionDto response = userInfoService.getPermission(userId);
 
         return ResponseEntity.ok().body(response);
     }
 
-    @GetMapping("/mypage/info/personal")
-    public ResponseEntity<PersonalInfoDto> getPersonalInfo(HttpServletRequest httpServletRequest){
-        String userId = jwtProvider.extractUserId(httpServletRequest);
+    @GetMapping("/user/personal/{userId}")
+    public ResponseEntity<PersonalInfoDto> getPersonalInfo(String userId,
+                                                           HttpServletRequest httpServletRequest){
+        String signupId = jwtProvider.extractUserId(httpServletRequest);
         PersonalInfoDto response = userInfoService.getPersonalInfo(userId);
 
         return ResponseEntity.ok().body(response);
     }
 
-    @PostMapping("/mypage/info/personal")
-    public ResponseEntity<PersonalInfoDto> editPersonalInfo(@RequestBody PersonalInfoDto personalInfoDto, HttpServletRequest httpServletRequest){
-        String userId = jwtProvider.extractUserId(httpServletRequest);
-        PersonalInfoDto response = userInfoService.editPersonalInfo(userId, personalInfoDto);
+    @PostMapping("/user/personal")
+    public ResponseEntity<PersonalInfoDto> editPersonalInfo(@RequestBody PersonalInfoDto personalInfoDto,
+                                                            HttpServletRequest httpServletRequest){
+        String signupId = jwtProvider.extractUserId(httpServletRequest);
+        PersonalInfoDto response = userInfoService.editPersonalInfo(signupId, personalInfoDto);
 
         return ResponseEntity.ok().body(response);
     }
 
+    //러닝 스펙 조회 vi
+    @GetMapping("/user/running/vi/{userId}")
+    public ResponseEntity<ViRunningInfoDto> getViRunningInfo(@PathVariable String userId,
+                                                             HttpServletRequest httpServletRequest){
+        String signupId = jwtProvider.extractUserId(httpServletRequest);
+        ViRunningInfoDto response = userInfoService.getViRunningInfo(userId, signupId);
+
+        return ResponseEntity.ok().body(response);
+    }
+    //러닝 스펙 조회 guide
+    @GetMapping("/user/running/guide/{userId}")
+    public ResponseEntity<GuideRunningInfoDto> getGuideRunningInfo(@PathVariable String userId,
+                                                                   HttpServletRequest httpServletRequest){
+        String signupId = jwtProvider.extractUserId(httpServletRequest);
+        GuideRunningInfoDto response = userInfoService.getGuideRunningInfo(signupId);
+
+        return ResponseEntity.ok().body(response);
+    }
 }
