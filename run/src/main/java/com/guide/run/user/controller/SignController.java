d76e806f176eff9920bd64ae7bb2c38f4f9b69cf
2ralumin <2ralumin@naver.com>
2024-01-26 22:54:16 +0900

    refactor(SignController): log를 위한 어노테이션 추가

--- run/src/main/java/com/guide/run/user/controller/SignController.java
+++ run/src/main/java/com/guide/run/user/controller/SignController.java
@@ -14,6 +14,7 @@ import com.guide.run.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.naming.CommunicationException;
 
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
