9ad25fa06bd5e1014b954383dc44203e1fe1747f
2ralumin <2ralumin@naver.com>
2024-01-26 22:58:58 +0900

    refactor(User) : 유저 타입 필드 추가

--- run/src/main/java/com/guide/run/user/entity/User.java
+++ run/src/main/java/com/guide/run/user/entity/User.java
@@ -3,40 +3,41 @@ package com.guide.run.user.entity;
 
 import com.guide.run.global.entity.BaseEntity;
 import com.guide.run.user.entity.type.Role;
+import com.guide.run.user.entity.type.UserType;
+import com.nimbusds.openid.connect.sdk.claims.Gender;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-
-import java.time.LocalDateTime;
 
 
 @Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn
 @SuperBuilder
 public class User extends BaseEntity {
     @Column(unique = true, nullable = false)
-    private String uuid;
+    private String uuid; //todo : 이름 바꿔야 함..
 
     @Id
     private String userId;
 
     private String name;
+
     private String gender;
     private String phoneNumber;
     private boolean openNumber;
     private int age;
     private String detailRecord;
+
     private String recordDegree; //개인 기록
     @Enumerated(EnumType.STRING)
     private Role role;
-    //private List<Event> eventLists;
+
+    @Enumerated(EnumType.STRING)
+    private UserType type;
     private String snsId;
     private boolean openSns;
 
