ddb63f0ce96cc50a946820898995c69ea1af3652
2ralumin <2ralumin@naver.com>
2024-01-27 18:56:45 +0900

    feat(SignupInfoController) : 약관 동의 수정 구현 및 post에서 patch로 수정 메소드 변경, 러닝스펙 수정 컨트롤러 코드 추가

--- run/src/main/java/com/guide/run/user/controller/SignupInfoController.java
+++ run/src/main/java/com/guide/run/user/controller/SignupInfoController.java
@@ -18,6 +18,7 @@ public class SignupInfoController {
     private final SignupInfoService signupInfoService;
     private final JwtProvider jwtProvider;
 
+    //약관 동의 조회
     @GetMapping("/user/permission/{userId}")
     public ResponseEntity<PermissionDto> getPermission(String userId){
         PermissionDto response = signupInfoService.getPermission(userId);
@@ -25,6 +26,17 @@ public class SignupInfoController {
         return ResponseEntity.ok().body(response);
     }
 
+    //약관 동의 수정
+    @PatchMapping("/user/permission")
+    public ResponseEntity<PermissionDto> editPermission(@RequestBody PermissionDto request,
+                                                        HttpServletRequest httpServletRequest){
+        String privateId = jwtProvider.extractUserId(httpServletRequest);
+        PermissionDto response = signupInfoService.editPermission(privateId, request);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    //인적사항 조회
     @GetMapping("/user/personal/{userId}")
     public ResponseEntity<PersonalInfoDto> getPersonalInfo(String userId){
         PersonalInfoDto response = signupInfoService.getPersonalInfo(userId);
@@ -32,7 +44,8 @@ public class SignupInfoController {
         return ResponseEntity.ok().body(response);
     }
 
-    @PostMapping("/user/personal")
+    //인적사항 수정
+    @PatchMapping("/user/personal")
     public ResponseEntity<PersonalInfoDto> editPersonalInfo(@RequestBody PersonalInfoDto personalInfoDto,
                                                             HttpServletRequest httpServletRequest){
         String privateId = jwtProvider.extractUserId(httpServletRequest);
@@ -55,4 +68,24 @@ public class SignupInfoController {
 
         return ResponseEntity.ok().body(response);
     }
+
+    //vi 러닝스펙 수정
+    @PatchMapping("/user/running/vi")
+    public ResponseEntity<ViRunningInfoDto> editViRunningInfo(HttpServletRequest httpServletRequest,
+                                                              @RequestBody ViRunningInfoDto request){
+        String privateId = jwtProvider.extractUserId(httpServletRequest);
+        ViRunningInfoDto response = signupInfoService.editViRunningInfo(privateId, request);
+        return ResponseEntity.ok().body(response);
+    }
+
+    //guide 러닝 스펙 수정
+    @PatchMapping("/user/running/vi")
+    public ResponseEntity<GuideRunningInfoDto> editGuideRunningInfo(HttpServletRequest httpServletRequest,
+                                                              @RequestBody GuideRunningInfoDto request){
+        String privateId = jwtProvider.extractUserId(httpServletRequest);
+        GuideRunningInfoDto response = signupInfoService.editGuideRunningInfo(privateId, request);
+        return ResponseEntity.ok().body(response);
+    }
+
+
 }
