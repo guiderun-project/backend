4f348f3fbcbe2fdc716c5d696331541c74de5389
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2024-02-02 23:39:08 +0900

    feat(EventForm) 이벤트 신청서 생성 -이벤트 신청서 클래스 생성 및 repository 생성

---
+++ run/src/main/java/com/guide/run/event/entity/EventForm.java
@@ -0,0 +1,35 @@
+package com.guide.run.event.entity;
+
+import com.guide.run.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EventForm extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //신청서 id
+    private String privateId; // 신청한 사람 id
+    private Long eventId;//신청한 이벤트 id
+    private String type; // 신청자 타입 임시로 String으로 해둠
+    private int age;
+    private String gender;
+    private String hopeTeam; //훈련 희망 팀
+    private String hopePartner; //희망 파트너 이름
+    private String referContent; // 운영진 참고내용
+    private boolean isMatching; //대기 매칭 상태
+    private LocalDateTime startTime;//이벤트 시작일+ 시작시간
+    private LocalDateTime endTime;//이벤트 종료일 + 이벤트 종료 시간
+}
