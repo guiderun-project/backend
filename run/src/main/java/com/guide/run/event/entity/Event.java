6f0f0cf22eda5d360b3d35f5e0825b4e9ea430dd
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2024-02-02 23:43:07 +0900

    fix(Event) 오타 수정 evenvt -> event

--- run/src/main/java/com/guide/run/event/entity/Event.java
+++ run/src/main/java/com/guide/run/event/entity/Event.java
@@ -1,7 +1,7 @@
 package com.guide.run.event.entity;
 
 import com.guide.run.event.entity.type.EventRecruitStatus;
-import com.guide.run.event.entity.type.EvenvtType;
+import com.guide.run.event.entity.type.EventType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -27,9 +27,9 @@ public class Event{
     private LocalDate recruitStartDate;//모집 시작일
     private LocalDate recruitEndDate;//모집 마감일
     private String name; //이벤트 제목
-    private EventRecruitStatus recruitStatus; // event 모집 상태 대기,승인,모집마감
+    private EventRecruitStatus recruitStatus; // event 모집 상태 대기,모집중,모집마감
     private boolean isApprove; // 이벤트 승인 여부
-    private EvenvtType type;//이벤트 분류
+    private EventType type;//이벤트 분류
     private LocalDateTime startTime;//이벤트 시작일+ 시작시간
     private LocalDateTime endTime;//이벤트 종료일 + 이벤트 종료 시간
     private int maxNumV;//vi 모집 인원
