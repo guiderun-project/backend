c85f49381bd2016096e189e2d1d24c4e145fa66b
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2024-02-08 04:45:59 +0900

    Refactor : Event 조회 관련 dto값 변경에 따른 변수명칭 변경

--- run/src/main/java/com/guide/run/event/service/EventGetService.java
+++ run/src/main/java/com/guide/run/event/service/EventGetService.java
@@ -1,8 +1,10 @@
-package com.guide.run.event.entity.service;
+package com.guide.run.event.service;
 
 import com.guide.run.event.entity.Event;
 import com.guide.run.event.entity.EventForm;
 import com.guide.run.event.entity.dto.response.search.MyEventResponse;
+import com.guide.run.event.entity.dto.response.search.MyEvent;
+import com.guide.run.event.entity.dto.response.search.UpcomingEvent;
 import com.guide.run.event.entity.dto.response.search.UpcomingEventResponse;
 import com.guide.run.event.entity.repository.EventFormRepository;
 import com.guide.run.event.entity.repository.EventRepository;
@@ -19,12 +21,12 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class EventSearchService {
+public class EventGetService {
     private final EventRepository eventRepository;
     private final EventFormRepository eventFormRepository;
 
-    public List<MyEventResponse> getMyEvent(String sort, int year, int month, String privateId) {
-        List<MyEventResponse> myEvents = new ArrayList<>();
+    public MyEventResponse getMyEvent(String sort, int year, int month, String privateId) {
+        List<MyEvent> myEvents = new ArrayList<>();
         LocalDateTime startTime = LocalDateTime.of(year, month, 1, 0, 0);
         int cnt = 4;
         LocalDateTime endTime = LocalDateTime.of(year, month, LocalDate.of(year, month, 1).lengthOfMonth(), 23, 59);
@@ -35,7 +37,7 @@ public class EventSearchService {
                 if (eventForm.isMatching()) {
                     Event findEvent = eventRepository.findByIdAndRecruitStatus(eventForm.getEventId(), EventRecruitStatus.OPEN);
                     if (findEvent != null) {
-                        myEvents.add(MyEventResponse.builder()
+                        myEvents.add(MyEvent.builder()
                                 .eventId(findEvent.getId())
                                 .eventType(findEvent.getType())
                                 .name(findEvent.getName())
@@ -48,14 +50,16 @@ public class EventSearchService {
                 }
                 if (cnt <= 0) break;
             }
-            return myEvents;
+            return MyEventResponse.builder()
+                    .items(myEvents)
+                    .build();
         } else if (sort.equals("CLOSE")) {
             myEventForms = eventFormRepository.findByPrivateIdAndEndTimeBetweenOrderByEndTimeDesc(privateId, startTime, endTime);
             for (EventForm eventForm : myEventForms) {
                 if (eventForm.isMatching()) {
                     Event findEvent = eventRepository.findByIdAndRecruitStatus(eventForm.getEventId(), EventRecruitStatus.CLOSE);
                     if (findEvent != null) {
-                        myEvents.add(MyEventResponse.builder()
+                        myEvents.add(MyEvent.builder()
                                 .eventId(findEvent.getId())
                                 .eventType(findEvent.getType())
                                 .name(findEvent.getName())
@@ -68,31 +72,37 @@ public class EventSearchService {
                 }
                 if (cnt <= 0) break;
             }
-            return myEvents;
+            return MyEventResponse.builder()
+                    .items(myEvents)
+                    .build();
         }
         throw new NotValidSortException();
     }
 
-    public List<UpcomingEventResponse> getUpcomingEvent(String sort, String privateId) {
-        List<UpcomingEventResponse> events = new ArrayList<>();
+    public UpcomingEventResponse getUpcomingEvent(String sort, String privateId) {
+        List<UpcomingEvent> events = new ArrayList<>();
         int cnt = 4;
         if (sort.equals("OPEN")) {
             List<Event> findEvents = eventRepository.findByRecruitStatusOrderByEndTime(EventRecruitStatus.OPEN);
             upcomingResponseCreate(privateId, events, cnt, findEvents);
-            return events;
+            return UpcomingEventResponse.builder()
+                    .items(events)
+                    .build();
         } else if (sort.equals("UPCOMING")) {
             List<Event> findEvents = eventRepository.findByRecruitStatusOrderByEndTime(EventRecruitStatus.UPCOMING);
             upcomingResponseCreate(privateId, events, cnt, findEvents);
-            return events;
+            return UpcomingEventResponse.builder()
+                    .items(events)
+                    .build();
         }
         throw new NotValidSortException();
     }
 
-    private void upcomingResponseCreate(String privateId, List<UpcomingEventResponse> events, int cnt, List<Event> findEvents) {
+    private void upcomingResponseCreate(String privateId, List<UpcomingEvent> events, int cnt, List<Event> findEvents) {
         for (Event e : findEvents) {
             EventForm eventForm = eventFormRepository.findByEventIdAndPrivateId(e.getId(), privateId);
             if (eventForm != null) {
-                events.add(UpcomingEventResponse.builder()
+                events.add(UpcomingEvent.builder()
                         .eventId(e.getId())
                         .eventType(e.getType())
                         .name(e.getName())
@@ -101,7 +111,7 @@ public class EventSearchService {
                         .recruitStatus(e.getRecruitStatus())
                         .build());
             } else {
-                events.add(UpcomingEventResponse.builder()
+                events.add(UpcomingEvent.builder()
                         .eventId(e.getId())
                         .eventType(e.getType())
                         .name(e.getName())
