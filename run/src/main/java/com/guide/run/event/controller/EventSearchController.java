a6ed466595ad2290b6d34067187838b6f56a51c9
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2024-02-09 23:15:54 +0900

    Feat : 전체 이벤트 조회 구현

--- run/src/main/java/com/guide/run/event/controller/EventSearchController.java
+++ run/src/main/java/com/guide/run/event/controller/EventSearchController.java
@@ -1,13 +1,15 @@
 package com.guide.run.event.controller;
 
 
+import com.guide.run.event.entity.dto.response.search.AllEvent;
 import com.guide.run.event.entity.dto.response.search.MyEventResponse;
 import com.guide.run.event.entity.dto.response.search.UpcomingEventResponse;
 import com.guide.run.event.entity.repository.EventRepository;
-import com.guide.run.event.entity.service.EventSearchService;
+import com.guide.run.event.service.AllEventGetService;
+import com.guide.run.event.service.EventGetService;
+import com.guide.run.event.entity.type.EventType;
 import com.guide.run.global.exception.user.resource.NotExistUserException;
 import com.guide.run.global.jwt.JwtProvider;
-import com.guide.run.user.entity.User;
 import com.guide.run.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -24,28 +26,56 @@ import java.util.List;
 @RequestMapping("/api/event")
 public class EventSearchController {
     private final JwtProvider jwtProvider;
-    private final EventSearchService eventSearchService;
+    private final EventGetService eventGetService;
     private final UserRepository userRepository;
+    private final AllEventGetService allEventGetService;
     private final EventRepository eventRepository;
 
     @GetMapping("/my")
-    public ResponseEntity<List<MyEventResponse>> getMyEventList(@RequestParam("sort") String sort
+    public ResponseEntity<MyEventResponse> getMyEventList(@RequestParam("sort") String sort
     , @RequestParam("year") int year, @RequestParam("month") int month, HttpServletRequest request)
     {
         String privateId = jwtProvider.extractUserId(request);
         userRepository.findByUserId(privateId).
                 orElseThrow(() -> new NotExistUserException());
-        List<MyEventResponse> myEvents = eventSearchService.getMyEvent(sort, year, month,privateId);
+        MyEventResponse myEvents = eventGetService.getMyEvent(sort, year, month, privateId);
         return ResponseEntity.status(200).body(myEvents);
     }
     @GetMapping("/upcoming")
-    public ResponseEntity<List<UpcomingEventResponse>> getUpcomingEventList(@RequestParam("sort") String sort,
-                                                                            HttpServletRequest request)
+    public ResponseEntity<UpcomingEventResponse> getUpcomingEventList(@RequestParam("sort") String sort,
+                                                                    HttpServletRequest request)
     {
         String privateId = jwtProvider.extractUserId(request);
         userRepository.findByUserId(privateId).
                 orElseThrow(() -> new NotExistUserException());
-        List<UpcomingEventResponse> upcomingEvents = eventSearchService.getUpcomingEvent(sort, privateId);
+        UpcomingEventResponse upcomingEvents = eventGetService.getUpcomingEvent(sort, privateId);
         return ResponseEntity.status(200).body(upcomingEvents);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<AllEvent>> getAllEventList(@RequestParam("start") int start,
+                                                          @RequestParam("limit") int limit,
+                                                          @RequestParam("sort") String sort,
+                                                          @RequestParam("type") String type,
+                                                          @RequestParam("kind") String kind,
+                                                          HttpServletRequest request)
+    {
+        String privateId = jwtProvider.extractUserId(request);
+        userRepository.findByUserId(privateId).
+                orElseThrow(() -> new NotExistUserException());
+        List<AllEvent> allEvents = allEventGetService.getAllEvent(start,limit,sort,type,kind,privateId);
+        return ResponseEntity.status(200).body(allEvents);
+    }
+    @GetMapping("/all/count")
+    public ResponseEntity<Integer> getEventListCount(@RequestParam("sort") String sort,
+                                                     @RequestParam("type") String type,
+                                                     @RequestParam("kind") String kind,
+                                                     HttpServletRequest request)
+    {
+        String privateId = jwtProvider.extractUserId(request);
+        userRepository.findByUserId(privateId).
+                orElseThrow(() -> new NotExistUserException());
+        int count = allEventGetService.getEventsListCount(sort,type,kind,privateId);
+        return ResponseEntity.status(200).body(count);
     }
 }
---
+++ run/src/main/java/com/guide/run/event/service/AllEventGetService.java
@@ -0,0 +1,349 @@
+package com.guide.run.event.service;
+
+import com.guide.run.event.entity.Event;
+import com.guide.run.event.entity.EventForm;
+import com.guide.run.event.entity.dto.response.search.AllEvent;
+import com.guide.run.event.entity.repository.EventFormRepository;
+import com.guide.run.event.entity.repository.EventRepository;
+import com.guide.run.event.entity.type.EventRecruitStatus;
+import com.guide.run.event.entity.type.EventType;
+import com.guide.run.global.exception.event.logic.NotValidKindException;
+import com.guide.run.global.exception.event.logic.NotValidSortException;
+import com.guide.run.global.exception.event.logic.NotValidTypeException;
+import com.guide.run.global.exception.event.resource.NotExistEventException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class AllEventGetService {
+    private final EventRepository eventRepository;
+    private final EventFormRepository eventFormRepository;
+
+    public List<AllEvent> getAllEvent(int start, int limit, String sort, String type, String kind, String privateId) {
+        Pageable pageable = PageRequest.of(start,limit);
+        List<AllEvent> events = new ArrayList<>();
+        switch (kind){
+            case "UPCOMING":
+                if(sort.equals("TOTAL")){
+                    if(type.equals("TOTAL")) {
+                        Page<Event> found = eventRepository.
+                                findAllByRecruitStatusNotOrderByEndTime(EventRecruitStatus.END, pageable);
+                        allResponseEventCreate(privateId,events,found);
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")) {
+                        Page<Event> found = eventRepository.
+                                findAllByTypeAndRecruitStatusNotOrderByEndTime(EventType.valueOf(type), EventRecruitStatus.END, pageable);
+                        allResponseEventCreate(privateId,events,found);
+                    }
+                    else
+                        throw new NotValidTypeException();
+                }
+                else if(sort.equals("UPCOMING")){
+                    if(type.equals("TOTAL")) {
+                        Page<Event> found = eventRepository.
+                                findAllByRecruitStatusOrderByEndTime(EventRecruitStatus.UPCOMING, pageable);
+                        allResponseEventCreate(privateId,events,found);
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")) {
+                        System.out.println("==========================================a");
+                        Page<Event> found = eventRepository.
+                                findAllByTypeAndRecruitStatusOrderByEndTime(EventType.valueOf(type), EventRecruitStatus.UPCOMING, pageable);
+                        System.out.println("=================================bb");
+                        allResponseEventCreate(privateId,events,found);
+                    }
+                    else
+                        throw new NotValidTypeException();
+                }
+                else if(sort.equals("OPEN")){
+                    if(type.equals("TOTAL")) {
+                        Page<Event> found = eventRepository.
+                                findAllByRecruitStatusOrderByEndTime(EventRecruitStatus.OPEN, pageable);
+                        allResponseEventCreate(privateId,events,found);
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")) {
+                        Page<Event> found = eventRepository.
+                                findAllByTypeAndRecruitStatusOrderByEndTime(EventType.valueOf(type), EventRecruitStatus.OPEN, pageable);
+                        allResponseEventCreate(privateId,events,found);
+                    }
+                    else
+                        throw new NotValidTypeException();
+                }
+                else if(sort.equals("CLOSE")){
+                    if(type.equals("TOTAL")) {
+                        Page<Event> found = eventRepository.
+                                findAllByRecruitStatusOrderByEndTime(EventRecruitStatus.CLOSE, pageable);
+                        allResponseEventCreate(privateId,events,found);
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")) {
+                        Page<Event> found = eventRepository.
+                                findAllByTypeAndRecruitStatusOrderByEndTime(EventType.valueOf(type), EventRecruitStatus.CLOSE, pageable);
+                        allResponseEventCreate(privateId,events,found);
+                    }
+                    else
+                        throw new NotValidTypeException();
+                }
+                else
+                    throw new NotValidSortException();
+                return events;
+            case "MY":
+                if(sort.equals("TOTAL")){
+                    if(type.equals("TOTAL")) {
+                        Page<EventForm> found = eventFormRepository.findAllByPrivateIdOrderByEndTime(privateId, pageable);
+                        myEventResponseCreate(events,found);
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")){
+                        Page<EventForm> found = eventFormRepository.
+                                findAllByPrivateIdAndEventTypeOrderByEndTime(privateId, EventType.valueOf(type), pageable);
+                        myEventResponseCreate(events,found);
+                    }
+                    else
+                        throw new NotValidTypeException();
+                }
+                else if(sort.equals("UPCOMING")){
+                    if(type.equals("TOTAL")) {
+                        Page<EventForm> found = eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatusOrderByEndTime(privateId, EventRecruitStatus.UPCOMING, pageable);
+                        myEventResponseCreate(events,found);
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")){
+                        Page<EventForm> found = eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatusAndEventTypeOrderByEndTime(privateId,
+                                        EventRecruitStatus.UPCOMING, EventType.valueOf(type), pageable);
+                        myEventResponseCreate(events,found);
+                    }
+                    else
+                        throw new NotValidTypeException();
+                }
+                else if(sort.equals("OPEN")){
+                    if(type.equals("TOTAL")) {
+                        Page<EventForm> found = eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatusOrderByEndTime(privateId, EventRecruitStatus.OPEN, pageable);
+                        myEventResponseCreate(events,found);
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")){
+                        Page<EventForm> found = eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatusAndEventTypeOrderByEndTime(privateId,
+                                        EventRecruitStatus.OPEN, EventType.valueOf(type), pageable);
+                        myEventResponseCreate(events,found);
+                    }
+                    else
+                        throw new NotValidTypeException();
+                }
+                else if(sort.equals("CLOSE")){
+                    if(type.equals("TOTAL")) {
+                        Page<EventForm> found = eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatusOrderByEndTime(privateId, EventRecruitStatus.CLOSE, pageable);
+                        myEventResponseCreate(events,found);
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")){
+                        Page<EventForm> found = eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatusAndEventTypeOrderByEndTime(privateId,
+                                        EventRecruitStatus.CLOSE, EventType.valueOf(type), pageable);
+                        myEventResponseCreate(events,found);
+                    }
+                    else
+                        throw new NotValidTypeException();
+                }
+                else if(sort.equals("END")){
+                    if(type.equals("TOTAL")) {
+                        Page<EventForm> found = eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatusOrderByEndTime(privateId, EventRecruitStatus.END, pageable);
+                        myEventResponseCreate(events,found);
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")){
+                        Page<EventForm> found = eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatusAndEventTypeOrderByEndTime(privateId,
+                                        EventRecruitStatus.END, EventType.valueOf(type), pageable);
+                        myEventResponseCreate(events,found);
+                    }
+                    else
+                        throw new NotValidTypeException();
+                }
+                else
+                    throw new NotValidSortException();
+                return events;
+            case "END":
+                if(type.equals("TOTAL")) {
+                    Page<Event> found = eventRepository.findAllByRecruitStatusOrderByEndTimeDesc(EventRecruitStatus.END, pageable);
+                    allResponseEventCreate(privateId, events, found);
+                }
+                else if(type.equals("TRAINING") || type.equals("COMPETITION")){
+                    Page<Event> found = eventRepository.findAllByTypeAndRecruitStatusOrderByEndTimeDesc(EventType.valueOf(type), EventRecruitStatus.END, pageable);
+                    allResponseEventCreate(privateId, events, found);
+                }
+                else
+                    throw new NotValidTypeException();
+                return events;
+            default:
+                throw new NotValidKindException();
+        }
+    }
+
+    private void myEventResponseCreate(List<AllEvent> events, Page<EventForm> found) {
+        for(EventForm ef : found){
+            Event e = eventRepository.findById(ef.getEventId()).orElseThrow(() -> new NotExistEventException());
+            events.add(AllEvent.builder()
+                    .eventId(e.getId())
+                    .eventType(e.getType())
+                    .name(e.getName())
+                    .isApply(true)
+                    .endDate(e.getEndTime().toLocalDate())
+                    .recruitStatus(e.getRecruitStatus())
+                    .build());
+        }
+    }
+
+    private void allResponseEventCreate(String privateId, List<AllEvent> events, Page<Event> found) {
+        for(Event e: found){
+            EventForm eventForm = eventFormRepository.findByEventIdAndPrivateId(e.getId(), privateId);
+            if(eventForm!=null) {
+                allEventCreate(events, e,true);
+            }else{
+                allEventCreate(events, e,false);
+            }
+        }
+    }
+
+    private void allEventCreate(List<AllEvent> events, Event e, Boolean isApply) {
+        events.add(AllEvent.builder()
+                .eventId(e.getId())
+                .eventType(e.getType())
+                .name(e.getName())
+                .isApply(isApply)
+                .endDate(e.getEndTime().toLocalDate())
+                .recruitStatus(e.getRecruitStatus())
+                .build());
+    }
+
+    public int getEventsListCount(String sort, String type, String kind, String privateId){
+        switch (kind){
+            case "UPCOMING":
+                if(sort.equals("TOTAL")){
+                    if(type.equals("TOTAL")) {
+                        return eventRepository.
+                                findAllByRecruitStatusNot(EventRecruitStatus.END).size();
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")) {
+                        return eventRepository.
+                                findAllByTypeAndRecruitStatusNot(EventType.valueOf(type), EventRecruitStatus.END).size();
+                    }
+                    else
+                        throw new NotValidTypeException();
+                }
+                else if(sort.equals("UPCOMING")){
+                    if(type.equals("TOTAL")) {
+                        return eventRepository.
+                                findAllByRecruitStatus(EventRecruitStatus.UPCOMING).size();
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")) {
+                        return eventRepository.
+                                findAllByTypeAndRecruitStatus(EventType.valueOf(type), EventRecruitStatus.UPCOMING).size();
+                    }
+                    else
+                        throw new NotValidTypeException();
+                }
+                else if(sort.equals("OPEN")){
+                    if(type.equals("TOTAL")) {
+                        return eventRepository.
+                                findAllByRecruitStatus(EventRecruitStatus.OPEN).size();
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")) {
+                        return eventRepository.
+                                findAllByTypeAndRecruitStatus(EventType.valueOf(type), EventRecruitStatus.OPEN).size();
+                    }
+                    else
+                        throw new NotValidTypeException();
+                }
+                else if(sort.equals("CLOSE")){
+                    if(type.equals("TOTAL")) {
+                        return eventRepository.
+                                findAllByRecruitStatus(EventRecruitStatus.CLOSE).size();
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")) {
+                        return eventRepository.
+                                findAllByTypeAndRecruitStatus(EventType.valueOf(type), EventRecruitStatus.CLOSE).size();
+                    }
+                    else
+                        throw new NotValidTypeException();
+                }
+                else
+                    throw new NotValidSortException();
+
+
+            case "MY":
+                if(sort.equals("TOTAL")){
+                    if(type.equals("TOTAL")) {
+                        return eventFormRepository.findAllByPrivateId(privateId).size();
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")){
+                        return eventFormRepository.
+                                findAllByPrivateIdAndEventType(privateId, EventType.valueOf(type)).size();
+                    }
+                }
+                else if(sort.equals("UPCOMING")){
+                    if(type.equals("TOTAL")) {
+                        return eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatus(privateId, EventRecruitStatus.UPCOMING).size();
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")){
+                        return eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatusAndEventType(privateId,
+                                        EventRecruitStatus.UPCOMING, EventType.valueOf(type)).size();
+                    }
+                }
+                else if(sort.equals("OPEN")){
+                    if(type.equals("TOTAL")) {
+                        return eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatus(privateId, EventRecruitStatus.OPEN).size();
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")){
+                        return eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatusAndEventType(privateId,
+                                        EventRecruitStatus.OPEN, EventType.valueOf(type)).size();
+                    }
+                }
+                else if(sort.equals("CLOSE")){
+                    if(type.equals("TOTAL")) {
+                        return eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatus(privateId, EventRecruitStatus.CLOSE).size();
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")){
+                        return eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatusAndEventType(privateId,
+                                        EventRecruitStatus.CLOSE, EventType.valueOf(type)).size();
+                    }
+                }
+                else if(sort.equals("END")){
+                    if(type.equals("TOTAL")) {
+                        return eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatus(privateId, EventRecruitStatus.END).size();
+                    }
+                    else if(type.equals("TRAINING") || type.equals("COMPETITION")){
+                        return eventFormRepository.
+                                findAllByPrivateIdAndRecruitStatusAndEventType(privateId,
+                                        EventRecruitStatus.END, EventType.valueOf(type)).size();
+                    }
+                }
+                else
+                    throw new NotValidSortException();
+
+            case "END":
+                if(type.equals("TOTAL")) {
+                    return eventRepository.findAllByRecruitStatus(EventRecruitStatus.END).size();
+                }
+                else if(type.equals("TRAINING") || type.equals("COMPETITION")){
+                    return eventRepository.findAllByTypeAndRecruitStatus(EventType.valueOf(type), EventRecruitStatus.END).size();
+                }
+            default:
+                throw new NotValidKindException();
+        }
+    }
+}
