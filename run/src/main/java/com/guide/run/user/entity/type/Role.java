7bf903ea374bae40fb457b97360e1e0f07fb6b71
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2024-02-12 02:46:06 +0900

    Refactor : 충돌 부분 수정

--- run/src/main/java/com/guide/run/user/entity/type/Role.java
+++ run/src/main/java/com/guide/run/user/entity/type/Role.java
@@ -7,14 +7,10 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum Role {
-    VADMIN("ROLE_VADMIN"),
-    GADMIN("ROLE_GADMIN"),
-    VWAIT("ROLE_VWAIT"),
-    GWAIT("ROLE_GWAIT"),
-    VCOACH("ROLE_VCOACH"),
-    GCOACH("ROLE_GCOACH"),
-    VI("ROLE_VI"),
-    GUIDE("ROLE_GUIDE"),
-    NEW("ROLE_NEW");
+    ADMIN("ADMIN"),
+    WAIT("WAIT"),
+    COACH("COACH"),
+    USER("USER"),
+    NEW("NEW");
     private final String value;
 }
