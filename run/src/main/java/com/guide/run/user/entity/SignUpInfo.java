1ce7ce4f0f0e3c22dfbc3e97f46382b8e83e3755
2ralumin <2ralumin@naver.com>
2024-02-14 01:21:52 +0900

    feat(SignUpInfo): 비밀번호 암호화 및 비밀번호 체크 구현

--- run/src/main/java/com/guide/run/user/entity/SignUpInfo.java
+++ run/src/main/java/com/guide/run/user/entity/SignUpInfo.java
@@ -1,14 +1,30 @@
 package com.guide.run.user.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SignUpInfo {
     @Id
     private String privateId;
     @Column(unique = true)
     private String accountId;
     private String password;
+
+    public SignUpInfo hashPassword(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
+        return this;
+    }
+
+    public boolean checkPassword(String plainPassword, PasswordEncoder passwordEncoder) {
+        return passwordEncoder.matches(plainPassword, this.password);
+    }
 }
