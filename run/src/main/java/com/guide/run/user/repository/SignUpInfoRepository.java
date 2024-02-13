d49c862c385df5ef0c18ce5ca55d9f052eabcc7d
2ralumin <2ralumin@naver.com>
2024-02-14 01:44:41 +0900

    feat(SignUpInfoRepository): 아이디 중복 확인을 위한 쿼리메소드 추가

--- run/src/main/java/com/guide/run/user/repository/SignUpInfoRepository.java
+++ run/src/main/java/com/guide/run/user/repository/SignUpInfoRepository.java
@@ -3,5 +3,8 @@ package com.guide.run.user.repository;
 import com.guide.run.user.entity.SignUpInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SignUpInfoRepository extends JpaRepository<SignUpInfo, String> {
+    Optional<SignUpInfo> findByAccountId(String accountId);
 }
