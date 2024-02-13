b70563cd848e8a4515cc1e652b3a6d3ad6e5ae82
2ralumin <2ralumin@naver.com>
2024-02-14 00:36:06 +0900

    feat(ViSignUpDto): 회원가입 dto validation 추가
    message 부분은 아직 예외처리를 구현하지 않아서 확인용으로 남긴 것. 이후 구현할 예정!

--- run/src/main/java/com/guide/run/user/dto/ViSignupDto.java
+++ run/src/main/java/com/guide/run/user/dto/ViSignupDto.java
@@ -1,6 +1,6 @@
 package com.guide.run.user.dto;
 
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,21 +12,40 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ViSignupDto {
-    //private String accountId;
-    //private String password;
+
+    @NotBlank(message = "아이디를 입력하세요")
+    private String accountId;
+
+    @Size(min = 8, message = "비밀번호는 최소 8글자 이상이어야 합니다.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*]).{8,}$",
+            message = "비밀번호는 숫자, 영어, 특수문자를 모두 포함해야 합니다.")
+    private String password;
+
+    @NotBlank(message = "이름을 입력하세요")
     private String name;
+    @NotBlank(message = "성별을 입력하세요")
     private String gender;
+
     @Pattern(regexp = "^01(?:0|1|[6-9])(?:\\s|-)?(?:\\d{3}|\\d{4})?(?:\\s|-)?\\d{4}$",
-            message = "전화번호 형식 에러") //todo : 전화번호 형식 관련 에러코드 추가해야 함.
+            message = "전화번호 형식 에러")
     private String phoneNumber;
+
+    @NotNull(message = "전화번호 공개 여부 오류")
     private boolean openNumber;
+
+    @PositiveOrZero(message = "나이 입력 오류")
     private int age;
+
     private String detailRecord;
+
+    @NotBlank(message = "기록 입력 오류")
     private String recordDegree; //개인 기록
+
     private String snsId;
     private boolean openSns;
 
     //vi 전용 정보
+    @NotNull(message = "러닝 경험 입력 오류")
     private boolean runningExp;
     private String guideName;
 
@@ -36,6 +55,8 @@ public class ViSignupDto {
     private String motive; //러닝 경험 있을 시 null
 
     //약관동의
+    @NotNull(message = "약관 동의 입력 오류")
     private boolean privacy;
+    @NotNull(message = "약관 동의 입력 오류")
     private boolean portraitRights;
 }
