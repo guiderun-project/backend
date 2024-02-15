9a43a3afc5a0396b45eea0bcd7b51ea44564d2f5
2ralumin <2ralumin@naver.com>
2024-02-15 20:06:57 +0900

    feat(AttDTO) : 출석 업로드용 dto

---
+++ run/src/main/java/com/guide/run/temp/member/dto/AttDTO.java
@@ -0,0 +1,20 @@
+package com.guide.run.temp.member.dto;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AttDTO {
+    private Long privateId;
+    private Long eventId;
+    private boolean isAttend;
+    private LocalDateTime date;
+}
