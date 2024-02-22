6147f9c451e95c4b9059fe22f0aace5f4e322405
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2024-02-22 22:53:37 +0900

    Feat(EventHistoryCount) : 회원 이벤트 히스토리 개수 응답값 구현

---
+++ run/src/main/java/com/guide/run/ap/EventHistoryCount.java
@@ -0,0 +1,12 @@
+package com.guide.run.ap;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Builder
+@Getter
+public class EventHistoryCount {
+    private int count;
+}
