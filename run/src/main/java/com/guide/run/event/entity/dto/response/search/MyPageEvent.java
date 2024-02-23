9077763833466ab7f78165e70a7ad614ecb07ead
2ralumin <2ralumin@naver.com>
2024-02-23 23:23:29 +0900

    Feature : 마이페이지 이벤트 조회용 dto

---
+++ run/src/main/java/com/guide/run/event/entity/dto/response/search/MyPageEvent.java
@@ -0,0 +1,22 @@
+package com.guide.run.event.entity.dto.response.search;
+
+import com.guide.run.event.entity.type.EventRecruitStatus;
+import com.guide.run.event.entity.type.EventType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyPageEvent {
+    private Long eventId;
+    private EventType eventType;
+    private String name;
+    private LocalDate date;
+    private EventRecruitStatus recruitStatus;
+}
---
+++ run/src/main/java/com/guide/run/event/entity/dto/response/search/MyPageEventResponse.java
@@ -0,0 +1,18 @@
+package com.guide.run.event.entity.dto.response.search;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyPageEventResponse {
+    private int start;
+    private int limit;
+    private List<MyPageEvent> items;
+}
