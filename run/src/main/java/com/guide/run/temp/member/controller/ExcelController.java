59ee26a8a656cb2cfa493ccee9a5e2152fd99322
2ralumin <2ralumin@naver.com>
2024-02-23 23:18:50 +0900

    Feature : 엑셀 정보 db 저장 코드 수정

--- run/src/main/java/com/guide/run/temp/member/controller/ExcelController.java
+++ run/src/main/java/com/guide/run/temp/member/controller/ExcelController.java
@@ -5,7 +5,7 @@ import com.guide.run.temp.member.dto.AttDTO;
 import com.guide.run.temp.member.dto.EventDTO;
 import com.guide.run.temp.member.dto.MemberDTO;
 import com.guide.run.temp.member.service.ExcelService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,11 +17,10 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class ExcelController {
 
-    @Autowired
-    private ExcelService excelService;
-
+    private final ExcelService excelService;
     @PostMapping("/member-upload")
     public ResponseEntity<String> uploadMemberExcelFile(@RequestParam("file") MultipartFile file) {
         if (file.isEmpty()) {
@@ -78,4 +77,5 @@ public class ExcelController {
             return new ResponseEntity<>("실패 " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
 }
