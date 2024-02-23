1ecaee42be340af9586e34ff43457562ac042f80
2ralumin <2ralumin@naver.com>
2024-02-23 23:24:34 +0900

    Feature : 어드민 서비스 구현

---
+++ run/src/main/java/com/guide/run/admin/service/AdminUserService.java
@@ -0,0 +1,147 @@
+package com.guide.run.admin.service;
+
+import com.guide.run.admin.dto.request.ApproveRequest;
+import com.guide.run.admin.dto.response.*;
+import com.guide.run.event.entity.dto.response.search.Count;
+import com.guide.run.global.converter.StringListConverter;
+import com.guide.run.global.exception.user.resource.NotExistUserException;
+import com.guide.run.user.entity.ArchiveData;
+import com.guide.run.user.entity.type.Role;
+import com.guide.run.user.entity.user.Guide;
+import com.guide.run.user.entity.user.User;
+import com.guide.run.user.entity.user.Vi;
+import com.guide.run.user.repository.ArchiveDataRepository;
+import com.guide.run.user.repository.GuideRepository;
+import com.guide.run.user.repository.UserRepository;
+import com.guide.run.user.repository.ViRepository;
+import jakarta.persistence.Convert;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AdminUserService {
+    private final UserRepository userRepository;
+    private final ViRepository viRepository;
+    private final GuideRepository guideRepository;
+    private final ArchiveDataRepository archiveDataRepository;
+
+    public UserListResponse getUserList(int start, int limit){
+        int page = start / limit;
+        Pageable pageable = PageRequest.of(page, limit, Sort.by("updatedAt").descending());
+        Page<User> userList = userRepository.findAll(pageable);
+        List<UserItem> userItems = new ArrayList<>();
+        for(User user : userList){
+            if(user.getRole()==Role.NEW){
+                continue;
+            }
+            UserItem item = UserItem.builder()
+                    .name(user.getName())
+                    .gender(user.getGender())
+                    .role(user.getRole())
+                    .phoneNumber(user.getPhoneNumber())
+                    .snsId(user.getSnsId())
+                    .totalCnt(user.getCompetitionCnt() + user.getTrainingCnt())
+                    .competitionCnt(user.getCompetitionCnt())
+                    .trainingCnt(user.getTrainingCnt())
+                    .team(user.getRecordDegree())
+                    .type(user.getType())
+                    .userId(user.getUserId())
+                    .update_date(user.getUpdatedAt().toLocalDate())
+                    .update_time(user.getUpdatedAt().toLocalTime())
+                    .build();
+            userItems.add(item);
+        }
+
+        UserListResponse response = UserListResponse.builder()
+                .items(userItems)
+                .limit(limit)
+                .start(start)
+                .build();
+        return response;
+    }
+
+    public Count getUserListCount(){
+        Count response = Count.builder()
+                .count((int) userRepository.count())
+                .build();
+        return response;
+    }
+
+    public ViApplyResponse getApplyVi(String userId){
+        User user = userRepository.findUserByUserId(userId).orElseThrow(NotExistUserException::new);
+        String privateId = user.getPrivateId();
+        Vi vi = viRepository.findById(privateId).orElseThrow(NotExistUserException::new);
+        ArchiveData archiveData = archiveDataRepository.findById(privateId).orElseThrow(NotExistUserException::new);
+
+        ViApplyResponse response = ViApplyResponse.builder()
+                .phoneNumber(user.getPhoneNumber())
+                .age(user.getAge())
+                .snsId(user.getSnsId())
+
+                .isRunningExp(vi.isRunningExp())
+                .detailRecord(user.getDetailRecord())
+                .recordDegree(user.getRecordDegree())
+                .guideName(vi.getGuideName())
+
+                .hopePrefs(archiveData.getHopePrefs())
+
+                .howToKnow(archiveData.getHowToKnow())
+                .motive(archiveData.getMotive())
+                .privacy(archiveData.isPrivacy())
+                .portraitRights(archiveData.isPortraitRights())
+                .build();
+     return response;
+    }
+
+    public GuideApplyResponse getApplyGuide(String userId){
+        User user = userRepository.findUserByUserId(userId).orElseThrow(NotExistUserException::new);
+        String privateId = user.getPrivateId();
+        Guide guide = guideRepository.findById(privateId).orElseThrow(NotExistUserException::new);
+        ArchiveData archiveData = archiveDataRepository.findById(privateId).orElseThrow(NotExistUserException::new);
+
+        GuideApplyResponse response = GuideApplyResponse.builder()
+                .phoneNumber(user.getPhoneNumber())
+                .age(user.getAge())
+                .snsId(user.getSnsId())
+
+                .isGuideExp(guide.isGuideExp())
+                .recordDegree(user.getRecordDegree())
+                .detailRecord(user.getDetailRecord())
+                .viCount(guide.getViCount())
+                .guidingPace(guide.getGuidingPace())
+                .runningPlace(archiveData.getRunningPlace())
+                .hopePrefs(archiveData.getHopePrefs())
+
+                .howToKnow(archiveData.getHowToKnow())
+                .motive(archiveData.getMotive())
+
+                .privacy(archiveData.isPrivacy())
+                .snsId(user.getSnsId())
+                .build();
+        return response;
+    }
+
+    @Transactional
+    public UserRoleResponse approveUser(String userId, ApproveRequest request){
+        User user = userRepository.findUserByUserId(userId).orElseThrow(NotExistUserException::new);
+        if(request.getIsApprove()){
+            user.editUserRole(Role.USER);
+        }else{
+            user.editUserRole(Role.REJECT);
+        }
+        UserRoleResponse response = new UserRoleResponse(user.getRole());
+        return response;
+    }
+
+}
