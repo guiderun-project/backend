efff8ea573d97c8e9e37847c0f8339fe3df08c9e
배두리 <78518155+2ralumin@users.noreply.github.com>
2024-03-13 11:09:32 +0900

    Update SecurityConfig.java

--- run/src/main/java/com/guide/run/global/security/config/SecurityConfig.java
+++ run/src/main/java/com/guide/run/global/security/config/SecurityConfig.java
@@ -54,12 +54,12 @@ public class SecurityConfig {
                 .sessionManagement(httpSecuritySessionManagementConfigurer ->
                         httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/api/test").hasRole("VADMIN")
-                        .requestMatchers("/api/test2").hasRole("VWAIT")
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN") //일단 각주 처리
+                        //.requestMatchers("/api/test").hasRole("VADMIN")
+                        //.requestMatchers("/api/test2").hasRole("VWAIT")
+                        //.requestMatchers("/api/admin/**").hasRole("ADMIN") //일단 각주 처리
                         .requestMatchers("/api/signup/duplicated").permitAll()
-                        .requestMatchers("/api/signup/**").hasRole("NEW")
-                        .requestMatchers("/api/user").hasRole("USER")
+                        //.requestMatchers("/api/signup/**").hasRole("NEW")
+                        //.requestMatchers("/api/user").hasRole("USER")
                         .requestMatchers("/api/test3").authenticated()
                         .anyRequest().permitAll())
 
