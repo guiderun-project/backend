896827fb1764aa94e06e3eed8440de3d7729258b
2ralumin <2ralumin@naver.com>
2024-04-21 23:59:46 +0900

    refactor : 이벤트 분류 대문자 소문자로 수정

--- run/src/main/java/com/guide/run/event/entity/dto/request/EventUpdateRequest.java
+++ run/src/main/java/com/guide/run/event/entity/dto/request/EventUpdateRequest.java
@@ -13,7 +13,7 @@ public class EventUpdateRequest {
     private LocalDate recruitStartDate;//모집 시작일
     private LocalDate recruitEndDate;//모집 마감일
     private String title;//이벤트 제목
-    private EventType EventType;//이벤트 분류
+    private EventType eventType;//이벤트 분류
     private LocalDateTime startTime;//이벤트 시작일+ 시작시간
     private LocalDateTime endTime;//이벤트 종료일 + 이벤트 종료 시간
     private int maxNumV;//vi 모집 인원
