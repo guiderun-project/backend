e76f4c6212840aba135d4006424cf996d12191ba
2ralumin <2ralumin@naver.com>
2024-06-06 12:30:45 +0900

    feat : 어드민 이벤트 기능 controller 생성

--- run/src/main/java/com/guide/run/admin/controller/AdminEventController.java
+++ run/src/main/java/com/guide/run/admin/controller/AdminEventController.java
@@ -1,9 +1,14 @@
 package com.guide.run.admin.controller;
 
-import com.guide.run.admin.dto.EventHistoryDto;
 import com.guide.run.admin.dto.EventTypeCountDto;
+import com.guide.run.admin.dto.condition.EventSortCond;
+import com.guide.run.admin.dto.request.ApprovalEvent;
+import com.guide.run.admin.dto.response.event.AdminEventHistoryList;
+import com.guide.run.admin.dto.response.event.AdminEventList;
+import com.guide.run.admin.dto.response.event.CurrentEventResponse;
+import com.guide.run.admin.dto.response.event.AdminEventResult;
 import com.guide.run.admin.service.AdminEventService;
-import com.guide.run.event.entity.dto.response.EventCountResponse;
+import com.guide.run.event.entity.dto.response.get.Count;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,42 +18,82 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
-public class AdminEventController {/*
+public class AdminEventController {
 
     private final AdminEventService adminEventService;
-    @GetMapping("/{userId}/event-list")
-    public ResponseEntity<EventHistoriesResponse> getUserEventHistories(@PathVariable String userId,
-                                                                        @RequestParam("start") int start,
-                                                                        @RequestParam("limit") int limit,
-                                                                        @RequestParam("year") int year,
-                                                                        @RequestParam("month") int month){
-        List<EventHistoryDto> eventHistoryDto = adminEventService.getEventHistory(userId, start, limit, year, month);
-        return ResponseEntity.status(200).body(EventHistoriesResponse.builder()
-                .items(eventHistoryDto)
-                .build());
-    }
-    @GetMapping("/{userId}/event-list/count")
-    public ResponseEntity<EventHistoryCountDto> getUserEventHistoryCount(@PathVariable String userId,
-                                                                         @RequestParam("year") int year,
-                                                                         @RequestParam("month") int month){
+
+    //전체 이벤트
+    @GetMapping("/event-list/count")
+    public ResponseEntity<Count> getAllEventCount(){
         return ResponseEntity.status(200).
-                body(EventHistoryCountDto.builder().count(adminEventService.getEventHistoryCount(userId,year,month)).build());
+                body(adminEventService.getAllEventCount());
+    }
+    @GetMapping("/event-list")
+    public ResponseEntity<AdminEventList> getAllEvent(@RequestParam("start") int start,
+                                                      @RequestParam("limit") int limit,
+                                                      @RequestParam(defaultValue = "false") boolean time,
+                                                      @RequestParam(defaultValue = "false") boolean name,
+                                                      @RequestParam(defaultValue = "false") boolean organizer,
+                                                      @RequestParam(defaultValue = "false") boolean approval
+                                                 ){
+
+        EventSortCond cond = EventSortCond.builder()
+                .time(time)
+                .approval(approval)
+                .name(name)
+                .organizer(organizer)
+                .build();
+        AdminEventList response = AdminEventList.builder()
+                .items(adminEventService.getAllEventList(start, limit, cond))
+                .build();
+        return ResponseEntity.status(200).body(response);
     }
 
-    @GetMapping("/{userId}/event-type/count")
+    @GetMapping("/current-event")
+    public ResponseEntity<List<CurrentEventResponse>> getCurrentEvent(@RequestParam("start") int start,
+                                                                      @RequestParam("limit") int limit){
+        List<CurrentEventResponse> response = adminEventService.getCurrentEvents(start, limit);
+        return ResponseEntity.ok(response);
+    }
+
+    //유저 이벤트 히스토리
+    @GetMapping("/event-list/{userId}")
+    public ResponseEntity<AdminEventHistoryList> getUserEventHistories(@PathVariable String userId,
+                                                                       @RequestParam("start") int start,
+                                                                       @RequestParam("limit") int limit,
+                                                                       @RequestParam(defaultValue = "all") String sort)
+    {
+        AdminEventHistoryList response = AdminEventHistoryList.builder()
+                .items(adminEventService.getEventHistory(userId, start, limit, sort))
+                .build();
+        return ResponseEntity.status(200).body(response);
+    }
+
+    @GetMapping("/event-list/count/{userId}")
+    public ResponseEntity<Count> getUserEventHistoryCount(@PathVariable String userId,
+                                                          @RequestParam(defaultValue = "all") String sort){
+        Count response = adminEventService.getEventHistoryCount(userId, sort);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/event-type/count/{userId}")
     public ResponseEntity<EventTypeCountDto> getUserEventHistoryCount(@PathVariable String userId){
         return ResponseEntity.status(200).
                 body(adminEventService.getEventTypeCount(userId));
     }
 
-    @GetMapping("/event-list/count")
-    public ResponseEntity<EventCountResponse> getUserEventHistoryCount(){
-        return ResponseEntity.status(200).
-                body(EventCountResponse.builder().count(adminEventService.getAllEventCount()).build());
+    //단일 이벤트
+    @PostMapping("/approval-event/{eventId}")
+    public ResponseEntity<String> approvalEvent(@PathVariable long eventId,
+                                                @RequestBody ApprovalEvent approvalEvent){
+        adminEventService.approvalEvent(eventId, approvalEvent);
+        return ResponseEntity.ok().body("");
     }
-    @GetMapping("/event-list")
-    public ResponseEntity<AllEventListResponse> getAllEvent(@RequestParam("start") int start,
-                                                            @RequestParam("limit") int limit){
-        return ResponseEntity.status(200).body(AllEventListResponse.builder().items(adminEventService.getAllEventList(start,limit)).build());
-    }*/
+
+    @GetMapping("event-result/{eventId}")
+    public ResponseEntity<AdminEventResult> getEventResult(@PathVariable long eventId){
+        return ResponseEntity.ok(adminEventService.getEventResult(eventId));
+    }
+
+
 }
