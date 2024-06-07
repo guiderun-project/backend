685dd2e079ab4019819b38cfcbb0d31fa74d6f3d
2ralumin <2ralumin@naver.com>
2024-06-07 15:53:48 +0900

    feat : 네이버 클라우드 sms 설정(테스트 못함)

---
+++ run/src/main/java/com/guide/run/global/naverCloud/dto/MessageDto.java
@@ -0,0 +1,12 @@
+package com.guide.run.global.naverCloud.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class MessageDto {
+    String to;
+}
