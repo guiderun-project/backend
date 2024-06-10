af1d72eabd18611e76e27fbdd6ebc946b7d41c8a
2ralumin <2ralumin@naver.com>
2024-06-10 12:37:49 +0900

    refactor : 불필요한 코드 삭제

--- run/src/main/java/com/guide/run/user/controller/SignupInfoController.java
+++ run/src/main/java/com/guide/run/user/controller/SignupInfoController.java
@@ -11,9 +11,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-@CrossOrigin(origins = {"https://guide-run-qa.netlify.app", "https://guiderun.org",
-        "https://guide-run.netlify.app","https://www.guiderun.org", "http://localhost:3000"},
-        maxAge = 3600)
 @Slf4j
 @RestController
 @RequiredArgsConstructor
