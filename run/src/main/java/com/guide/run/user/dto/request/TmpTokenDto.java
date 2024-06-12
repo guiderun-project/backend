2dfc0a65dd35d599acc4223a37038d56e5bb570b
2ralumin <2ralumin@naver.com>
2024-06-12 10:51:39 +0900

    fix : 아이디 찾기 시 토큰 에러 수정

---
+++ run/src/main/java/com/guide/run/user/dto/request/TmpTokenDto.java
@@ -0,0 +1,14 @@
+package com.guide.run.user.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class TmpTokenDto {
+    private String token;
+}
