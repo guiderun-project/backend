bceb7e8b36453aa909c7ebafee6ff8dff3b218d8
2ralumin <2ralumin@naver.com>
2024-06-29 17:40:40 +0900

    feat : sms api 연결

---
+++ run/src/main/java/com/guide/run/global/sms/cool/CoolSMS.java
@@ -0,0 +1,39 @@
+package com.guide.run.global.sms.cool;
+
+import jakarta.annotation.PostConstruct;
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Value;
+
+public class CoolSMS {
+    @Value("${spring.coolsms.senderNumber}")
+    private String senderNumber;
+
+    @Value("${spring.coolsms.apiKey}")
+    private String apiKey;
+
+    @Value("${spring.coolsms.apiSecret}")
+    private String apiSecret;
+
+    private DefaultMessageService messageService;
+
+    @PostConstruct
+    private void init(){
+        this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecret, "https://api.coolsms.co.kr");
+    }
+
+    // 단일 메시지 발송 예제
+    public SingleMessageSentResponse sendOne(String to, String verificationCode) {
+        Message message = new Message();
+        // 발신번호 및 수신번호는 반드시 01012345678 형태
+        message.setFrom(senderNumber);
+        message.setTo(to);
+        message.setText("[Guide Run Project] 인증번호를 입력해주세요\n" + verificationCode);
+
+        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
+        return response;
+    }
+}
