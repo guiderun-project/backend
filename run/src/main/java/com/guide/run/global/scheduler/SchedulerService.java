fbd3567f5a63d462c70cf6e830d30d7cc6041e97
2ralumin <2ralumin@naver.com>
2024-06-30 13:32:39 +0900

    fix : 스케줄러 모집 마감 날짜 처리

--- run/src/main/java/com/guide/run/global/scheduler/SchedulerService.java
+++ run/src/main/java/com/guide/run/global/scheduler/SchedulerService.java
@@ -71,8 +71,6 @@ public class SchedulerService {
         LocalDate today = LocalDate.now();
         LocalDateTime now = LocalDateTime.now();
 
-
-
         if(schedule!=null){
             schedule.changeTime(
                     event.getStartTime(),
@@ -87,7 +85,7 @@ public class SchedulerService {
                     .eventEnd(event.getEndTime())
                     .recruitStart(event.getRecruitStartDate())
                     //모집 종료는 하루 더해야 함.
-                    .recruitEnd(event.getRecruitEndDate())
+                    .recruitEnd(event.getRecruitEndDate().plusDays(1))
                     .eventStatus(ScheduleStatus.PENDING)
                     .recruitStatus(ScheduleStatus.PENDING)
                     .build();
@@ -113,16 +111,19 @@ public class SchedulerService {
         LocalDate today = LocalDate.now();
         LocalDateTime now = LocalDateTime.now();
 
-        if(schedule.getRecruitStart().isEqual(today) || schedule.getRecruitStart().isAfter(today)){
+        if(schedule.getRecruitStart().isEqual(today) || schedule.getRecruitStart().isAfter(today)
+            || schedule.getRecruitStatus().equals(ScheduleStatus.PENDING)){
             addRecruitStartTask(schedule);
         }
-        else if(schedule.getRecruitEnd().isEqual(today) || schedule.getRecruitEnd().isAfter(today)){
+        else if(schedule.getRecruitEnd().isEqual(today) || schedule.getRecruitEnd().isAfter(today)
+            || schedule.getRecruitStatus().equals(ScheduleStatus.OPEN)){
             addRecruitEndTask(schedule);
         }
-        else if(schedule.getEventStart().isEqual(now) || schedule.getEventStart().isAfter(now)){
+        else if(schedule.getEventStart().isEqual(now) || schedule.getEventStart().isAfter(now)
+        || schedule.getEventStatus().equals(ScheduleStatus.PENDING)){
            addEventStartTask(schedule);
         }
-        else if(schedule.getEventStart().isBefore(now)){
+        else if(schedule.getEventStart().isBefore(now) || schedule.getEventStatus().equals(ScheduleStatus.OPEN)){
             addEventEndTask(schedule);
         }
     }
