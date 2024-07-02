5f5a168f2483c7bdcdd2fef38659c4c81bc29714
2ralumin <2ralumin@naver.com>
2024-07-02 11:35:20 +0900

    fix : 토큰 삭제 처리 추가

--- run/src/main/java/com/guide/run/global/redis/AuthNumberRepository.java
+++ run/src/main/java/com/guide/run/global/redis/AuthNumberRepository.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface AuthNumberRepository extends CrudRepository<AuthNumber, String> {
     Optional<AuthNumber> findByAuthNum(String authNum);
+    void deleteAuthNumberByAuthNum(String authNum);
 }
