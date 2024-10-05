2ae92e2b237e78d00df1918bc4c8ea97c94ec0ad
2ralumin <2ralumin@naver.com>
2024-10-05 19:10:08 +0900

    fix : QEventForm import 문 추가 깃허브 반영 재시도

--- run/src/main/java/com/guide/run/temp/member/repository/AttendanceRepositoryImpl.java
+++ run/src/main/java/com/guide/run/temp/member/repository/AttendanceRepositoryImpl.java
@@ -1,5 +1,10 @@
 package com.guide.run.temp.member.repository;
 
+
+import static com.guide.run.event.entity.QEventForm.eventForm;
+import static com.guide.run.temp.member.entity.QAttendance.attendance;
+import static com.guide.run.user.entity.user.QUser.user;
+
 import com.guide.run.event.entity.dto.response.attend.ParticipationInfo;
 import com.guide.run.temp.member.entity.Attendance;
 import com.guide.run.user.entity.type.UserType;
@@ -10,9 +15,6 @@ import jakarta.persistence.EntityManager;
 import java.util.List;
 
 
-import static com.guide.run.event.entity.QEventForm.eventForm;
-import static com.guide.run.temp.member.entity.QAttendance.attendance;
-import static com.guide.run.user.entity.user.QUser.user;
 
 public class AttendanceRepositoryImpl implements AttendanceCustomRepository{
     private final JPAQueryFactory queryFactory;
