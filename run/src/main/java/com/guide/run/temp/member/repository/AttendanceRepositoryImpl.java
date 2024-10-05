d793a1e91be2a72ff180fed5eaae95a67834b8d1
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2024-10-05 17:01:10 +0900

    fix : 신청서 등급이 들어갈 부분에 회원 러닝 등급으로 되어 있는 오류 수정

--- run/src/main/java/com/guide/run/temp/member/repository/AttendanceRepositoryImpl.java
+++ run/src/main/java/com/guide/run/temp/member/repository/AttendanceRepositoryImpl.java
@@ -9,6 +9,7 @@ import jakarta.persistence.EntityManager;
 
 import java.util.List;
 
+import static com.guide.run.event.entity.QEventForm.eventForm;
 import static com.guide.run.temp.member.entity.QAttendance.*;
 import static com.guide.run.user.entity.user.QUser.*;
 
@@ -33,11 +34,11 @@ public class AttendanceRepositoryImpl implements AttendanceCustomRepository{
         return queryFactory.select(Projections.constructor(ParticipationInfo.class,
                 user.userId.as("userId"),
                 user.type.as("type"),
-                user.recordDegree.as("applyRecord"),
+                eventForm.hopeTeam.as("applyRecord"),
                 user.name.as("name")))
                 .from(attendance)
                 .join(user).on(attendance.privateId.eq(user.privateId))
-                .where(attendance.isAttend.eq(isAttend).and(attendance.eventId.eq(eventId)))
+                .where(attendance.isAttend.eq(isAttend).and(attendance.eventId.eq(eventId)).and(eventForm.eventId.eq(eventId)))
                 .orderBy(user.type.desc())
                 .fetch();
     }
