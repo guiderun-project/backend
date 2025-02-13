c5120106713f5fb0443d91123c9e2d56bbaec095
2ralumin <2ralumin@naver.com>
2025-02-14 04:46:13 +0900

    fix : 1365 id Dto 추가

---
+++ run/src/main/java/com/guide/run/user/dto/request/Add1365Dto.java
@@ -0,0 +1,10 @@
+package com.guide.run.user.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Add1365Dto {
+    private String id1365;
+}
