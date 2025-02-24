3f6fc98af040b0a553436774ee2a6d8324d07776
2ralumin <2ralumin@naver.com>
2025-02-24 17:00:51 +0900

    fix : null 예외 해결

--- run/src/main/java/com/guide/run/global/converter/SetStringConverter.java
+++ run/src/main/java/com/guide/run/global/converter/SetStringConverter.java
@@ -3,6 +3,7 @@ package com.guide.run.global.converter;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -25,7 +26,7 @@ public class SetStringConverter implements AttributeConverter<Set<Long>, String>
     @Override
     public Set<Long> convertToEntityAttribute(String s) {
         if (s == null || s.isEmpty()) {
-            return null;
+            return new HashSet<>();
         }
         // 쉼표로 구분된 문자열 각 요소를 Long으로 변환 후 Set으로 변경
         return Arrays.stream(s.split(SPLIT))
