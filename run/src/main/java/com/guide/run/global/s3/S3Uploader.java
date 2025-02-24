51e7fb502e5fa4686a3f3b9b921f37d3598d0756
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2025-02-24 13:51:14 +0900

    fix : s3 업로드시 삭제 추가

--- run/src/main/java/com/guide/run/global/s3/S3Uploader.java
+++ run/src/main/java/com/guide/run/global/s3/S3Uploader.java
@@ -89,8 +89,8 @@ public class S3Uploader {
             // URL 디코딩을 통해 원래의 파일 이름을 가져옴.
             //String decodedFileName = URLDecoder.decode(fileName, "UTF-8");
             log.info("Deleting file from S3: " + fileName);
-            log.info("file name is" + fileName.substring(cloudFrontUrl.length()-2));
-            amazonS3.deleteObject(bucket, fileName.substring(cloudFrontUrl.length()-2));
+            log.info("file name is" + fileName.substring(cloudFrontUrl.length()));
+            amazonS3.deleteObject(bucket, fileName.substring(cloudFrontUrl.length()));
         }
         catch (Exception e) {
             log.error("Error while decoding the file name: {}", e.getMessage());
