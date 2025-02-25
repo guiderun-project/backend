c363bc89df75ec7dcd98ba73fe0e2d9629676968
2ralumin <2ralumin@naver.com>
2025-02-25 17:07:25 +0900

    fix : 파트너 찾기 쿼리 오류 수정(중복 제거)

--- run/src/main/java/com/guide/run/partner/entity/partner/repository/PartnerRepositoryImpl.java
+++ run/src/main/java/com/guide/run/partner/entity/partner/repository/PartnerRepositoryImpl.java
@@ -32,7 +32,7 @@ public class PartnerRepositoryImpl implements PartnerRepositoryCustom {
     @Override
     public List<MyPagePartner> findMyPartner(String privateId, String sort, int limit, int start, UserType userType) {
         return queryFactory
-                .select(Projections.constructor(MyPagePartner.class,
+                .selectDistinct(Projections.constructor(MyPagePartner.class,
                         user.userId,
                         user.img,
                         user.role,
@@ -67,7 +67,7 @@ public class PartnerRepositoryImpl implements PartnerRepositoryCustom {
     @Override
     public long countMyPartner(String privateId, UserType userType) {
         Long count = queryFactory
-                .select(partner.count())
+                .selectDistinct(partner.count())
                 .from(partner)
                 .where(
                         partnerUserWhere(userType, privateId),
@@ -80,7 +80,7 @@ public class PartnerRepositoryImpl implements PartnerRepositoryCustom {
     @Override
     public List<AdminPartnerResponse> getAdminPartner(String privateId, UserType type, String kind, int limit, int start) {
         return queryFactory
-                .select(Projections.constructor(AdminPartnerResponse.class,
+                .selectDistinct(Projections.constructor(AdminPartnerResponse.class,
                         user.userId,
                         user.img,
                         user.role,
@@ -107,7 +107,7 @@ public class PartnerRepositoryImpl implements PartnerRepositoryCustom {
     @Override
     public long countAdminPartner(String privateId, UserType type, String kind) {
         Long count = queryFactory
-                .select(partner.count())
+                .selectDistinct(partner.count())
                 .from(partner)
                 .where(
                         partnerUserWhere(type, privateId),
@@ -120,7 +120,7 @@ public class PartnerRepositoryImpl implements PartnerRepositoryCustom {
     @Override
     public List<AdminPartnerResponse> searchAdminPartner(String privateId, UserType type, String text, int limit, int start) {
         return queryFactory
-                .select(Projections.constructor(AdminPartnerResponse.class,
+                .selectDistinct(Projections.constructor(AdminPartnerResponse.class,
                         user.userId,
                         user.img,
                         user.role,
@@ -147,7 +147,7 @@ public class PartnerRepositoryImpl implements PartnerRepositoryCustom {
     @Override
     public long searchAdminPartnerCount(String privateId, UserType type, String text) {
         Long count = queryFactory
-                .select(partner.count())
+                .selectDistinct(partner.count())
                 .from(partner)
                 .join(user).on(partnerUserJoin(type))
                 .where(
@@ -161,7 +161,7 @@ public class PartnerRepositoryImpl implements PartnerRepositoryCustom {
     @Override
     public List<AttendAndPartnerDto> getEndEventAttendanceAndPartner(long eventId) {
         return queryFactory
-                .select(Projections.constructor(AttendAndPartnerDto.class,
+                .selectDistinct(Projections.constructor(AttendAndPartnerDto.class,
                         user.privateId,
                         user.type,
                         partner.viId,
