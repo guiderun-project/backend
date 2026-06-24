726b5a4c4bcd060e4bccf2eddb1f0420a53d859a
Pridesd <whwotjr98@naver.com>
2026-06-24 11:44:17 +0900

    fix: prioritize domain exception advice

--- run/src/main/java/com/guide/run/global/exception/auth/AuthAuthorizeExceptionAdvice.java
+++ run/src/main/java/com/guide/run/global/exception/auth/AuthAuthorizeExceptionAdvice.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RequiredArgsConstructor
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class AuthAuthorizeExceptionAdvice {
     private final MessageSource messageSource;
     private final ResponseService responseService;
